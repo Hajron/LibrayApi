044394196afb21f9719435ebee4020fcc4db19e3
Hajron <119761530+Hajron@users.noreply.github.com>
2024-12-09 11:26:41 +0100

    Added endpoints: HttpGet, HttpGet by Id, HttpPut by Id, HttpPost and  HttpDelete

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -11,7 +11,61 @@ namespace LibraryApi.Controllers
         public static List<Book> books = new List<Book>
         {
             new Book { Id = 1, Title = "Monster", Author = "Drew Mansion", Year = 1977 },
-            new Book { Id = 1, Title = "Bulls Eye", Author = "Camilla Colly", Year = 1960 }
+            new Book { Id = 2, Title = "Bulls Eye", Author = "Camilla Colly", Year = 1960 }
         };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {   
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> CreateBook(Book newBook)
+        {
+            newBook.Id = books.Max(b => b.Id) + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(CreateBook), new {newBook.Id, newBook});
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Book> UpdateBook(int id, Book updatedBook)
+        {
+            var existingBook = books.FirstOrDefault(b => b.Id == id);
+            if (existingBook == null)
+            {
+                return NoContent();
+            }
+            existingBook.Title = updatedBook.Title;
+            existingBook.Author = updatedBook.Author;
+            existingBook.Year = updatedBook.Year;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult DeleteBook(int id)
+        {
+            var book = books.Find(b => b.Id == id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            books.Remove(book);
+            return NoContent();
+        }
     }
 }
